0a525e32e100189ad951cb10c84949a73eed0c50
salicodes <kamsangsali@gmail.com>
2019-06-06 16:41:04 +0100

    Multiple version build

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/#!/usr/bin/env bash
 set -eu
 rm *.pdf -f
-pandoc *.md -o example.pdf --from markdown --template spdx-pdf-template --table-of-contents
+for BRANCH in `git branch --list|sed 's/\*//g'`;
+  do
+    git checkout $BRANCH
+    git fetch
+    cd $BRANCH/chapters/
+    pandoc *.md -o example.pdf --from markdown --template spdx-pdf-template --table-of-contents
+  done
+git checkout master;
