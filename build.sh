2f158cc6a3bf5ba5e680b8c8912cc3e9bb7eb0ae
salicodes <kamsangsali@gmail.com>
2019-06-14 05:03:31 +0100

    set current working directory to the directory of the script

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ for BRANCH in `git branch --list|sed 's/\*//g'`;
   do
     git checkout $BRANCH
     # git fetch
-    cd $BRANCH/chapters/
+    cd "$(dirname "$0")"
+    cd chapters
     pandoc *.md -o example.pdf --from markdown --template spdx-pdf-template --table-of-contents
+    cd ..
   done
 git checkout master;
